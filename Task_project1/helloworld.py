46237da1e79fd306131e9cc1916eb63ba57ffb25
Ron-96 <136598153+Ron-96@users.noreply.github.com>
2023-07-05 17:29:25 +0200

    added a if statement for user age

--- Task_project1/helloworld.py
+++ Task_project1/helloworld.py
@@ -3,6 +3,7 @@ inp = "What is your name ? "
 
 if inp == "John":
     print("Hello John")
-
+if age == "25":
+    print("You are 25")
 else: 
-    print("You are not John, please leave")
+    print("You are not John and you are not 25 years old, please leave")
